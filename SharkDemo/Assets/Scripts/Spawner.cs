c23738ab09ce2b09d0dd394ca609d77438c6655b
Garrick Chan <changarrickt@gmail.com>
2025-08-29 23:24:05 -0700

    Added spline animation

--- SharkDemo/Assets/Scripts/Spawner.cs
+++ SharkDemo/Assets/Scripts/Spawner.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
+using UnityEngine.Splines;
 
 public class Spawner : MonoBehaviour
 {
     public GameObject[] creatures;
+    public GameObject[] splines;
     int creatureCount;
-    GameObject current;
+    GameObject currentCreature;
+    GameObject currentSpline;
+    SplineAnimate ani;
 
     Vector3 start_pos = new Vector3(-20f, 0f, 0f);
 
@@ -18,18 +22,29 @@ public class Spawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(current.transform.position.x > 20f)
+        // if(current.transform.position.x > 20f)
+        // {
+        //     Destroy(currentCreature);
+        //     Spawn();
+        // }
+        // current.transform.Translate(Vector3.forward * 1f * Time.deltaTime);
+        if(ani.NormalizedTime >= 1.0f)
         {
-            Destroy(current);
+            Destroy(currentCreature);
+            Destroy(currentSpline);
             Spawn();
         }
-        current.transform.Translate(Vector3.forward * 1f * Time.deltaTime);
     }
 
     public void Spawn()
     {
-        current = Instantiate(creatures[Random.Range(0, creatureCount)]);
-        current.transform.Rotate(0f, 90f, 0f);
-        current.transform.position = start_pos;
+        int index = Random.Range(0, creatureCount);
+        currentCreature = Instantiate(creatures[index]);
+        currentSpline = Instantiate(splines[index]);
+        ani = currentCreature.GetComponent<SplineAnimate>();
+        ani.Container = currentSpline.GetComponent<SplineContainer>();
+        ani.Restart(true);
+        // current.transform.Rotate(0f, 90f, 0f);
+        // current.transform.position = start_pos;
     }
 }
