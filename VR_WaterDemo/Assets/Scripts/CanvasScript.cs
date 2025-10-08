9b3a43c6566d1949d68935b4ce494b88d613426d
Michael M <michael.martin.tech@gmail.com>
2025-10-08 15:24:27 -0700

    TEMP PATCH: Disabled lens distortion due to 'double vision' visual issue during testing phase. To be confirmed.

--- VR_WaterDemo/Assets/Scripts/CanvasScript.cs
+++ VR_WaterDemo/Assets/Scripts/CanvasScript.cs
@@ -10,6 +10,9 @@ public class Canvas : MonoBehaviour
     [SerializeField] private float fadeSpeed = 1f;
     private CanvasGroup cg;
 
+    // error below- fix toggleGUI
+    public bool toggleGUI;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -23,13 +26,19 @@ public class Canvas : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(mode == Mode.FadeIn && cg.alpha < 1f)
+        // Appear
+        
+        if (mode == Mode.FadeIn && cg.alpha < 1f)
         {
             cg.alpha = Mathf.Min(cg.alpha + fadeSpeed * Time.deltaTime, 1f);
             if(cg.alpha >= 1f) { mode = Mode.FadeOut; }     // delete this later
         } 
         else if(mode == Mode.FadeOut && cg.alpha > 0)
         {
+            if (toggleGUI == false)
+            {
+                return;
+            }
             cg.alpha = Mathf.Max(cg.alpha - fadeSpeed * Time.deltaTime, 0f);
             if(cg.alpha <= 0) { mode = Mode.FadeIn; }       // delete this later
         }
